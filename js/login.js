7198926d49e755947fcea9d1b4fa9d0af2622e4a
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2020-06-30 15:08:16 +0100

    - added popup modal - ceated a login screen - validation for 3 attemps to login

--- js/login.js
+++ js/login.js
@@ -1,10 +1,45 @@
+var Modalelem;
+var instance;
+
 window.onload = function () {
 
+    Modalelem = document.querySelector('.modalLogin');
+    instance = M.Modal.init(Modalelem, {
+        dismissible: false
+    });
+
+    //localStorage.setItem('test', 'Hello');
+    //token = localStorage.getItem('test')
 
 
-    instance.open();
+    if (token === '' || token === null || token === undefined) {
+        console.log('empty')
+        instance.open();
+    } else {
+        console.log('value is present')
+    }
 
+}
 
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+    var username = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    // if api call success 
+    if (username == "Formget@gmail.com" && password == "formget#123") {
+        instance.close();
+    }
+    else {
+        attempt--;// Decrementing by one.
+        M.toast({ html: "You have left " + attempt + " attempt" })
 
-
+        // Disabling fields after 3 attempts.
+        if (attempt == 0) {
+            document.getElementById("email").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
 }
