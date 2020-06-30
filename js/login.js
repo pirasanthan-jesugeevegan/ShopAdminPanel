ccf480812839eac3164a23cfd9badbfe2eee19dd
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2020-06-30 16:55:21 +0100

    -added login function -validation -passing token in to locatStorage

--- js/login.js
+++ js/login.js
@@ -1,15 +1,13 @@
 var Modalelem;
 var instance;
-
+var attempt = 3;
+var token
 window.onload = function () {
 
     Modalelem = document.querySelector('.modalLogin');
     instance = M.Modal.init(Modalelem, {
         dismissible: false
     });
-
-    //localStorage.setItem('test', 'Hello');
-    //token = localStorage.getItem('test')
 
 
     if (token === '' || token === null || token === undefined) {
@@ -21,14 +19,37 @@ window.onload = function () {
 
 }
 
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
-function validate() {
+
+
+function login() {
+
     var username = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    // if api call success 
+    var link = 'https://restful-shop-api.herokuapp.com/user/login';
+
+
     if (username == "Formget@gmail.com" && password == "formget#123") {
-        instance.close();
+        request.open('POST', link, true)
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(JSON.stringify({ "email": "test@test.com", "password": "tester" })); //repace email and testing with dynamic value 
+        request.onload = function () {
+            // Begin accessing JSON data here
+            var data = JSON.parse(this.response)
+            token = data.token
+            console.log(data.message)
+            if (request.status >= 200 && request.status < 400) {
+                M.toast({ html: data.message })
+                console.log(token)
+                localStorage.setItem('token', token);
+                //  token1 = localStorage.getItem('token')
+                // console.log(token1)
+                instance.close();
+            } else {
+                M.toast({ html: data.message })
+                //login()
+            }
+        }
+
     }
     else {
         attempt--;// Decrementing by one.
